9e35a71f33da734d7649a75bf75affd26b83dc7e
Ryan Kendrick <ryan@kendrickfamily.info>
2021-07-12 22:35:34 +0100

    TextVid modifications

--- TextVid/TextVid.py
+++ TextVid/TextVid.py
@@ -5,16 +5,18 @@ import sys
 text_overlay_json_path = "./TextOverlays.json"
 original_vid_path = "./NewVid.mp4"
 edited_vid_path = "./NewVid_Edited.mp4"
-text_overlays = []
 
-if len(sys.argv) >= 2:
+text_overlays = []
+num_of_args = len(sys.argv)
+
+if num_of_args >= 2:
     text_overlay_json_path = sys.argv[1]
 
-    if len(sys.argv) >= 3:
+    if num_of_args >= 3:
         original_vid_path = sys.argv[2]
-
-    if len(sys.argv) >= 4:
-        edited_vid_path = sys.argv[3]
+        
+        if num_of_args >= 4:
+            edited_vid_path = sys.argv[3]
 
 print("Preparing...")
 
@@ -22,20 +24,18 @@ with open(text_overlay_json_path) as json_file:
     text_overlays = json.load(json_file)
 
 video = VideoFileClip(original_vid_path)
-audio = AudioFileClip(original_vid_path) 
 video_clips = [video]
 
 for overlay in text_overlays:
     video_clips.append(
-        TextClip(overlay["Text"],fontsize=70,color='white')
+        TextClip(overlay["Text"], fontsize=70, color='white')
             .set_position(overlay["Position"])
             .set_start(overlay["StartTime"])
             .set_end(overlay["StopTime"]))
 
-result = CompositeVideoClip(video_clips) # Overlay text on video
-result.set_audio(video.audio)
+result = CompositeVideoClip(video_clips).set_audio(video.audio) # Overlay text on video
 
 print("\nExporting...\n")
 
-result.write_videofile(edited_vid_path, codec='libx264', preset="slow",)
+result.write_videofile(edited_vid_path, codec='libx264', preset="slow")
 result.close()
